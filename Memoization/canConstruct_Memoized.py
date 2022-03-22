ee6ac1abece20935106ed992f0c352147d5ababb
mcwinters <70335876+mcwinters@users.noreply.github.com>
2022-03-22 15:36:35 -0400

    Update canConstruct_Memoized.py

--- Memoization/canConstruct_Memoized.py
+++ Memoization/canConstruct_Memoized.py
@@ -1,6 +1,6 @@
 """Write a function can_construct(target, wordBank) that accepts a target string and a list of strings. 
 The function should return a boolean indicating whether or not the target can be constructed by concatenating elements of the wordBank list. 
-You may reuse elements of the word_bank as many times as needed."""
+You may reuse elements of the wordBank as many times as needed."""
 
 def canConstruct(target, wordBank, cache = None):
     if cache == None: cache = {}  # Workaround to have new cache for each testcase
