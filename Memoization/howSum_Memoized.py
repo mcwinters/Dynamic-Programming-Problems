c96011b541bd28b21b58e046753ea65069c3e802
mcwinters <70335876+mcwinters@users.noreply.github.com>
2022-03-22 15:38:07 -0400

    Update howSum_Memoized.py

--- Memoization/howSum_Memoized.py
+++ Memoization/howSum_Memoized.py
@@ -1,6 +1,6 @@
-"""Write a function how_sum(target_sum, numbers) that takes in a target_sum and a list of numbers as arguments. 
-The function should return a list containing any combination of elements that add up to exactly the target_sum. 
-If there is no combination that adds up to the target_sum, then return None. If there are multiple combinations possible, you may return any single one. 
+"""Write a function how_sum(target, numbers) that takes in a target and a list of numbers as arguments. 
+The function should return a list containing any combination of elements that add up to exactly the target. 
+If there is no combination that adds up to the target, then return None. If there are multiple combinations possible, you may return any single one. 
 You may use an element of the list as many times as needed. You may assume that all input numbers are nonnegative."""
 
 def howSum(target, numbers, cache = None):
